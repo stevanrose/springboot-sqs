df349393637755832e0554a44d916599f5f3f9d1
Stevan Rose <stevan.rose@gmail.com>
2018-05-26 14:33:37 +0100

    NOJIRA Create a simple Spring Boot Amazon SQS sample project
    The application provides a simple rest endpoint to send a message to
    an Amazon SQS queue.

--- src/main/java/com/stevanrose/springsqs/SpringSqsApplication.java
+++ src/main/java/com/stevanrose/springsqs/SpringSqsApplication.java
@@ -1,11 +1,7 @@
 package com.stevanrose.springsqs;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.core.env.ResourceIdResolver;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSqsApplication {
@@ -14,8 +10,8 @@ public class SpringSqsApplication {
 		SpringApplication.run(SpringSqsApplication.class, args);
 	}
 
-	@Bean
-	public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSqs, ResourceIdResolver resourceIdResolver) {
-		return new QueueMessagingTemplate(amazonSqs, resourceIdResolver);
-	}
+//	@Bean
+//	public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSqs, ResourceIdResolver resourceIdResolver) {
+//		return new QueueMessagingTemplate(amazonSqs, resourceIdResolver);
+//	}
 }
